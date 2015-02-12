4641ebfcaf3a4043ed3967890bdc7c3b13c78145
Peter Green <peter.green@aztek-native.com>
2015-02-12 17:05:02 +0000

    Added attributes.

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+
+# Path to install the TW5 application
+default['tiddlywiki5']['tw5_path'] = "/srv/tw5"
+
+# Primary hostname for the platform
+default['tiddlywiki5']['site_hostname'] = "tiddlywiki5.com"
+
+# TCP port that the TiddlyWiki application will bind to
+default['tiddlywiki5']['tw5_port'] = 8080
+
