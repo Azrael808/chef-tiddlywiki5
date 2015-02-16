da52a7bbfbb0d246c9dc786ba648007859b137d5
Peter Green <peter.green@aztek-native.com>
2015-02-16 17:31:59 +0000

    Updated license.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'tiddlywiki5'
 maintainer       'AzTek Native Ltd.'
 maintainer_email 'peter.green@aztek-native.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures tiddlywiki5'
 long_description 'Installs/Configures tiddlywiki5'
 version          '0.1.0'
