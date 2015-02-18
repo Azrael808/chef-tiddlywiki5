54add19c12b838f0d152f04faeb1000edd29acc2
Peter Green <peter.green@aztek-native.com>
2015-02-18 14:26:06 +0000

    Don't configure ldap auth by default.

--- attributes/nodejs.rb
+++ attributes/nodejs.rb
@@ -4,7 +4,7 @@ default['tiddlywiki5']['nodejs']['nginx_proxy'] = true
 override['nginx']['install_method'] = 'source'
 
 # Do we want the nginx proxy to perform LDAP auth?
-default['tiddlywiki5']['nodejs']['ldap_auth'] = true
+default['tiddlywiki5']['nodejs']['ldap_auth'] = false 
 
 default['tiddlywiki5']['nodejs']['ldap_basedn'] = "dc=example,dc=com"
 
