608cb1a64fc5e165212a60b6f909140800162b6c
Peter Green <peter.green@aztek-native.com>
2015-02-18 16:24:09 +0000

    Updated metadata: added nginx_auth_ldap as dependency and incremented version.

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,11 @@ maintainer_email 'peter.green@aztek-native.com'
 license          'Apache 2.0'
 description      'Installs/Configures tiddlywiki5'
 long_description 'Installs/Configures tiddlywiki5'
-version          '0.1.0'
+version          '0.2.0'
 %w{ application
     application_nginx
-    application_nodejs }.each do |d|
+    application_nodejs
+    nginx_auth_ldap }.each do |d|
 
   depends          d
 end
