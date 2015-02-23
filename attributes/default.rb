c7bb2c9110c80cd724f995e6e2f32a6f356a80c1
Peter Green <peter.green@aztek-native.com>
2015-02-23 16:16:41 +0000

    Changed default site URL.

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ default['tiddlywiki5']['user'] = 'tw5'
 default['tiddlywiki5']['tw5_path'] = "/srv/tw5"
 
 # Primary hostname for the platform
-default['tiddlywiki5']['site_hostname'] = "tiddlywiki5.com"
+default['tiddlywiki5']['site_hostname'] = "tiddlywiki5.example.com"
 
 # TCP port that the TiddlyWiki application will bind to
 default['tiddlywiki5']['tw5_port'] = 8080
