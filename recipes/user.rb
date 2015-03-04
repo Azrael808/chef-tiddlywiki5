31603117d2f2089cf9ca8cf5b9dd5e199dada840
Peter Green <peter.green@aztek-native.com>
2015-03-04 14:54:03 +0000

    Added the creation of a home directory for the TW5 user.

--- recipes/user.rb
+++ recipes/user.rb
@@ -12,5 +12,14 @@ group node['tiddlywiki5']['user']
 user node['tiddlywiki5']['user'] do
   action :create
   group node['tiddlywiki5']['user']
+  home "/home/#{node['tiddlywiki5']['user']}"
+  shell "/bin/bash"
+end
+
+directory "/home/#{node['tiddlywiki5']['user']}" do
+  action :create
+  owner node['tiddlywiki5']['user']
+  group node['tiddlywiki5']['user']
+  mode "0700"
 end
 
